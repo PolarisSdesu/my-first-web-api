fc0567223f532b659b2b93c2cf8c1f9e3c088f0a
polaris <polarissdesu@outlook.com>
2024-02-19 16:10:45 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Api
+
+我使用 Golang 所写的第一个 web api，用来打印“Hello, World!”。
---
+++ webApi.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", corsWrapper(handler))
+	http.ListenAndServe(":8080", nil)
+}
+
+// handler 是原始的请求处理函数a
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json") // Content-Type 头部
+	fmt.Fprintf(w, `{"message": "Hello, World!"}`)
+}
+
+// corsWrapper 创建一个包装器函数来处理 CORS 头部信息
+func corsWrapper(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")             // 允许任何源
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS") // 允许 GET 和 OPTIONS 方法（这里仅做示例，实际应根据需求配置）
+
+		if r.Method == "OPTIONS" {
+			// 如果是预检（OPTIONS）请求，则结束处理并返回
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		// 调用原始的请求处理函数
+		h(w, r)
+	}
+}
